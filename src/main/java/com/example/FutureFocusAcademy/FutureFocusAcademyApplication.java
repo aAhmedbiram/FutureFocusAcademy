c8e70e04af6e8000bcfdffe3d71e245bb4bfa7d0
aAhmedbiram <ahmedbiram47@gmail.com>
2024-10-13 17:17:39 +0200

    Initial commit

---
+++ src/main/java/com/example/FutureFocusAcademy/FutureFocusAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.example.FutureFocusAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureFocusAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureFocusAcademyApplication.class, args);
+	}
+
+}
