8207148376df388579506fc3f787f2ce02fa95a9
kerolos119 <kerolosbotros99@gmail.com>
2024-11-05 00:31:31 +0200

    Update MapperConfiguration.java

--- src/main/java/com/example/FutureFocusAcademy/config/MapperConfiguration.java
+++ src/main/java/com/example/FutureFocusAcademy/config/MapperConfiguration.java
@@ -1,14 +1,14 @@
-//package com.example.FutureFocusAcademy.config;
-//
-//import org.modelmapper.ModelMapper;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class MapperConfiguration {
-//
-//    @Bean(name = "customModelMapper") // Rename the bean
-//    public ModelMapper modelMapper() {
-//        return new ModelMapper();
-//    }
-//}
+package com.example.FutureFocusAcademy.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfiguration {
+
+    @Bean(name = "customModelMapper") // Rename the bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
